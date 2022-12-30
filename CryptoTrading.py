e39edbfe8d07f908573aef94805a31e789e8ffb4
Brent Matterson <brentmatterson@gmail.com>
2022-12-30 11:35:08 -0800

    testing with blackjack

--- CryptoTrading.py
+++ CryptoTrading.py
@@ -4,17 +4,16 @@ import torch
 import torch.nn as nn
 from torch import Tensor
 from torch.distributions.normal import Normal
-from sklearn.preprocessing import MinMaxScaler
 
 import gymnasium as gym
-from gymnasium import spaces
-from gymnasium.utils import seeding
-from gymnasium.envs.registration import register
-from enum import Enum
+from collections import defaultdict
+from tqdm import tqdm
 
 import matplotlib.pyplot as plt
 
 from gym_examples.envs.trading_env import TradingEnv
+
+from sklearn.preprocessing import MinMaxScaler
 
 import math
 import requests
@@ -31,9 +30,9 @@ import time
 from talib.abstract import MACD, RSI, CCI, DX
 import talib as ta
 
-print("\n\n\n'88,dPYba,,adPYba,   ,adPPYba,  8b,dPPYba,   ,adPPYba, 8b       d8  \n","88P'   '88'    '8a a8'     '8a 88P'   `'8a a8P_____88 `8b     d8'  \n","88      88      88 8b       d8 88       88 8PP'''''''  `8b   d8'   \n","88      88      88 '8a,   ,a8' 88       88 '8b,   ,aa   `8b,d8'    \n","88      88      88  `'YbbdP''  88       88  `'Ybbd8''     Y88'     \n","                                                          d8'      \n","                                                         d8'       \n")
-print("> welcome to moneyDRL")
-print("> Creating Testing Data")
+
+print('\n'+'\n'+'\n'+"                                   )\._.,--....,'``.      " + '\n' + "                                  /;   _.. \   _\  (`._ ,." + '\n' + "                    $            `----(,_..'--(,_..'`-.;.'" + '\n' + '\n' + "                                                       /$$$$$$$  /$$$$$$$  /$$      " + "\n" +"                                                      | $$__  $$| $$__  $$| $$      " + "\n" +" /$$$$$$/$$$$   /$$$$$$  /$$$$$$$   /$$$$$$  /$$   /$$| $$  \ $$| $$  \ $$| $$      " + "\n" +"| $$_  $$_  $$ /$$__  $$| $$__  $$ /$$__  $$| $$  | $$| $$  | $$| $$$$$$$/| $$      " + "\n" +"| $$ \ $$ \ $$| $$  \ $$| $$  \ $$| $$$$$$$$| $$  | $$| $$  | $$| $$__  $$| $$      " + "\n" +"| $$ | $$ | $$| $$  | $$| $$  | $$| $$_____/| $$  | $$| $$  | $$| $$  \ $$| $$      " + "\n" +"| $$ | $$ | $$|  $$$$$$/| $$  | $$|  $$$$$$$|  $$$$$$$| $$$$$$$/| $$  | $$| $$$$$$$$" + "\n" +"|__/ |__/ |__/ \______/ |__/  |__/ \_______/ \____  $$|_______/ |__/  |__/|________/" + "\n" +"                                             /$$  | $$                              " + "\n" +"                                            |  $$$$$$/                              " + "\n" +"                                             \______/                               " + "\n" + "\n")
+print("creating Testing Data")
 
 TICKER_LIST = ["ETHUSDT"]#, "ATOMUSDT", "ADAUSDT", "BTCUSDT", "SOLUSDT", "DOTUSDT", "DOGEUSDT", "AVAXUSDT"]
 INDICATORS = ['high','low','open','close','fng','rsi','macd','macd_signal','macd_hist','cci','dx','rf','sar','adx','adxr','apo','aroonosc','bop','cmo','minus_di','minus_dm','mom','plus_di','plus_dm','ppo_ta','roc','rocp','rocr','rocr100','trix','ultosc','willr','ht_dcphase','ht_sine','ht_trendmode','feature_PvEWMA_4','feature_PvCHLR_4','feature_RvRHLR_4','feature_CON_4','feature_RACORR_4','feature_PvEWMA_8','feature_PvCHLR_8','feature_RvRHLR_8','feature_CON_8','feature_RACORR_8','feature_PvEWMA_16','feature_PvCHLR_16','feature_RvRHLR_16','feature_CON_16','feature_RACORR_16']
@@ -53,13 +52,13 @@ p.download_data(TICKER_LIST)
 p.clean_data()
 df = p.dataframe
 
-t = DataProcessor(data_source='binance', start_date=TEST_START_DATE, end_date=TEST_END_DATE, time_interval=TIME_INTERVAL)
-t.download_data(TICKER_LIST)
-t.clean_data()
-df_TEST = t.dataframe
+#t = DataProcessor(data_source='binance', start_date=TEST_START_DATE, end_date=TEST_END_DATE, time_interval=TIME_INTERVAL)
+#t.download_data(TICKER_LIST)
+#t.clean_data()
+#df_TEST = t.dataframe
 
 print(len(df))
-print(len(df_TEST))
+#print(len(df_TEST))
 
 def addFnG(df):
     #BASING EVERYTHING OFF THE BTC DATAFRAME
@@ -168,7 +167,7 @@ def addFnG(df):
     return df
 
 df = addFnG(df = df)
-df_TEST = addFnG(df = df_TEST)
+#df_TEST = addFnG(df = df_TEST)
 
 def add_feature_columns(df, period_length):
     # get the price vs ewma feature
@@ -231,18 +230,23 @@ def addIndicators(df):
 
         # prune the nan rows at the beginning of the dataframe
         tic_df = tic_df.dropna()
-        tic_df = tic_df[[column for column in tic_df.columns if column not in ['high', 'low']]]
 
         final_df = final_df.append(tic_df)
 
     df = final_df
     df.index=pd.to_datetime(df.time)
     df.drop('time', inplace=True, axis=1)
+    df.drop('tic', inplace=True, axis=1)
+
+    min_max_scaler = MinMaxScaler()
+    df[df.columns] = min_max_scaler.fit_transform(df[df.columns])
+
     df = df.dropna()
+
     return df
 
 df = addIndicators(df = df)
-df_TEST = addIndicators(df = df_TEST)
+#df_TEST = addIndicators(df = df_TEST)
 
 #def splitIntoArr(df): 
 #    unique_ticker = df.tic.unique()
@@ -270,22 +274,100 @@ df_TEST = addIndicators(df = df_TEST)
 
 env = gym.make('gym_examples/TradingEnv-v0', df = df, window_size = 10)
 
-observation = env.reset()
-print(observation)
+done = False
+
+price, state = env.reset()
+print(price)
+print(state)
+
+learning_rate = 0.01
+n_episodes = 100_000
+start_epsilon = 1.0
+epsilon_decay = start_epsilon / (n_episodes / 2)  # reduce the exploration over time
+final_epsilon = 0.1
+
+class BlackjackAgent:
+    def __init__( self, learning_rate: float, initial_epsilon: float, epsilon_decay: float, final_epsilon: float, discount_factor: float = 0.95):
+
+        self.q_values = defaultdict(lambda: np.zeros(env.action_space.n))
+
+        self.lr = learning_rate
+        self.discount_factor = discount_factor
+
+        self.epsilon = initial_epsilon
+        self.epsilon_decay = epsilon_decay
+        self.final_epsilon = final_epsilon
+
+        self.training_error = []
+
+    def get_action(self, obs: tuple[int, int, bool]) -> int:
+
+        # with probability epsilon return a random action to explore the environment
+        if np.random.random() < self.epsilon:
+            return env.action_space.sample()
+
+        # with probability (1 - epsilon) act greedily (exploit)
+        else:
+            return int(np.argmax(self.q_values[obs]))
+
+    def update( self, obs: tuple[int, int, bool], action: int, reward: float, terminated: bool, next_obs: tuple[int, int, bool],):
+        """Updates the Q-value of an action."""
+        future_q_value = (not terminated) * np.max(self.q_values[next_obs])
+        temporal_difference = (reward + self.discount_factor * future_q_value - self.q_values[obs][action])
+
+        self.q_values[obs][action] = (self.q_values[obs][action] + self.lr * temporal_difference)
+        self.training_error.append(temporal_difference)
+
+    def decay_epsilon(self):
+        self.epsilon = max(self.final_epsilon, self.epsilon - epsilon_decay)
 
 
+agent = BlackjackAgent(learning_rate=learning_rate, initial_epsilon=start_epsilon, epsilon_decay=epsilon_decay, final_epsilon=final_epsilon,)
 
+for episode in tqdm(range(n_episodes)):
+    obs, info = env.reset()
+    done = False
 
+    # play one episode
+    while not done:
+        action = agent.get_action(obs)
+        next_obs, reward, terminated, truncated, info = env.step(action)
 
+        # update the agent
+        agent.update(obs, action, reward, terminated, next_obs)
 
+        # update if the environment is done and the current obs
+        done = terminated or truncated
+        obs = next_obs
 
+    agent.decay_epsilon()
 
-
-
-
-
-
-
+rolling_length = 500
+fig, axs = plt.subplots(ncols=3, figsize=(12, 5))
+axs[0].set_title("Episode rewards")
+reward_moving_average = (
+    np.convolve(
+        np.array(env.return_queue).flatten(), np.ones(rolling_length), mode="valid"
+    )
+    / rolling_length
+)
+axs[0].plot(range(len(reward_moving_average)), reward_moving_average)
+axs[1].set_title("Episode lengths")
+length_moving_average = (
+    np.convolve(
+        np.array(env.length_queue).flatten(), np.ones(rolling_length), mode="same"
+    )
+    / rolling_length
+)
+axs[1].plot(range(len(length_moving_average)), length_moving_average)
+axs[2].set_title("Training Error")
+training_error_moving_average = (
+    np.convolve(np.array(agent.training_error), np.ones(rolling_length), mode="same")
+    / rolling_length
+)
+axs[2].plot(range(len(training_error_moving_average)), training_error_moving_average)
+plt.tight_layout()
+plt.show()
 
 
 
@@ -322,10 +404,6 @@ def sell(self):
             print(">> Declining sale: Not profitable to sell BTC")
     else:
         print(">> Not enough BTC left in your account to buy USD ")
-
-
-
-
 
 def to_ndarray(item, dtype):
     r"""
--- gym-examples/gym_examples/envs/trading_env.py
+++ gym-examples/gym_examples/envs/trading_env.py
@@ -1,16 +1,26 @@
-import gym
-from gym import spaces
+import gymnasium as gym
+from gymnasium import spaces
+from gymnasium.utils import seeding
+import random
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import numbers
+
+INDICATORS = ['high','low','open','close','fng','rsi','macd','macd_signal','macd_hist','cci','dx','rf','sar','adx','adxr','apo','aroonosc','bop','cmo','minus_di','minus_dm','mom','plus_di','plus_dm','ppo_ta','roc','rocp','rocr','rocr100','trix','ultosc','willr','ht_dcphase','ht_sine','ht_trendmode','feature_PvEWMA_4','feature_PvCHLR_4','feature_RvRHLR_4','feature_CON_4','feature_RACORR_4','feature_PvEWMA_8','feature_PvCHLR_8','feature_RvRHLR_8','feature_CON_8','feature_RACORR_8','feature_PvEWMA_16','feature_PvCHLR_16','feature_RvRHLR_16','feature_CON_16','feature_RACORR_16']
 
 class TradingEnv(gym.Env):
 
-    def __init__(self, df, render_mode=None, capital_frac = 0.2, cap_thresh=0.3, window_size = 10):
+    def __init__(self, df, capital_frac = 0.2, cap_thresh=0.3, window_size = 10):
+
+        self.seed()
         self.df = df
+        self.window_size = window_size
+        
         self.terminal_idx = len(self.df) - 1
 
-        #self.prices, self.signal_features, self.feature_dim_len = self._process_data()
+        self.selected_feature_name = INDICATORS
+        self.prices, self.signal_features, self.feature_dim_len = self._process_data()
 
         self._current_tick = 0
         self._done = None
@@ -18,7 +28,7 @@ class TradingEnv(gym.Env):
         self.current_price = None
 
         self.action_space = spaces.Discrete(2)
-        self.observation_space = spaces.Box(low = -np.inf, high = np.inf, shape = self.shape, dtype = np.float64)
+        self.observation_space = spaces.Box(low = -np.inf, high = np.inf, dtype = np.float64)
 
         self.capital_frac = capital_frac
         self.cap_thresh = cap_thresh
@@ -26,55 +36,59 @@ class TradingEnv(gym.Env):
         self.initial_capital = 1000
         self.portfolio_value = self.initial_capital
         self.running_capital = self.initial_capital
-        self.token_balance = 0
+        
+        self.initial_token_balance = 0
+        self.token_balance = self.initial_token_balance
 
         self.store = {"action_store": [], "reward_store": [], "running_capital": [], "port_ret": []}
 
-        assert render_mode is None
-        self.render_mode = render_mode
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
 
-    #def _process_data(self):
-    #    '''
-    #    Overview:
-    #        used by env.reset(), process the raw data.
-    #    Arguments:
-    #        - start_idx (int): the start tick; if None, then randomly select.
-    #    Returns:
-    #        - prices: the close.
-    #        - selected_feature: feature map
-    #        - feature_dim_len: the dimension length of selected feature
-    #    '''
-#
-    #    # ====== build feature map ========
-    #    all_feature = {k: self.df.loc[:, k].to_numpy() for k in INDICATORS}
-    #    prices = self.df.loc[:, 'close'].to_numpy()
-    #    # =================================
-#
-    #    # ====== select features ========
-    #    corr_matrix = pd.DataFrame(self.df).corr().abs()
-    #    upper_tri = corr_matrix.where(np.triu(np.ones(corr_matrix.shape),k=1).astype(np.bool));
-#
-    #    selected_feature_name = INDICATORS
-#
-    #    # selecting the columns which are having absolute correlation greater than 0.95 and making a list of those columns named 'dropping_these_features'.
-    #    dropping_these_features = [column for column in upper_tri.columns if any(upper_tri[column] > 0.99)]
-    #    for droppingFeature in dropping_these_features:
-    #        selected_feature_name.remove(str(droppingFeature))
-    #
-    #    selected_feature = np.column_stack([all_feature[k] for k in selected_feature_name])
-    #    feature_dim_len = len(selected_feature_name)
-#
-    #    return prices, selected_feature, feature_dim_len
+    def _process_data(self):
+        '''
+        Overview:
+            used by env.reset(), process the raw data.
+        Arguments:
+            - start_idx (int): the start tick; if None, then randomly select.
+        Returns:
+            - prices: the close.
+            - selected_feature: feature map
+            - feature_dim_len: the dimension length of selected feature
+        '''
+        prices = self.df.loc[:, 'close'].to_numpy()
+        
+        # ====== select features ========
+        corr_matrix = self.df.corr().abs()
+        upper_tri = corr_matrix.where(np.triu(np.ones(corr_matrix.shape),k=1).astype(np.bool));
+
+        # selecting the columns which are having absolute correlation greater than 0.95 and making a list of those columns named 'dropping_these_features'.
+        dropping_these_features = [column for column in upper_tri.columns if any(upper_tri[column] > 0.95)]
+        for droppingFeature in dropping_these_features:
+            self.selected_feature_name.remove(str(droppingFeature))
+
+        selected_feature = np.column_stack((self.df.loc[:, k].to_numpy() for k in self.selected_feature_name))
+        feature_dim_len = len(self.selected_feature_name)
+ 
+        self.selected_feature_name.append('port_ovr_init_cap')
+        self.selected_feature_name.append('run_ovr_port')
+        self.selected_feature_name.append('investmnt_ovr_init_cap')
+
+        return prices, selected_feature, feature_dim_len
 
     def reset(self):
+        self.portfolio_value = self.initial_capital
+        self.running_capital = self.initial_capital
+
+        self.token_balance = self.initial_token_balance
+
         self.store = {"action_store": [], "reward_store": [], "running_capital": [], "port_ret": []}
+        
         self.current_price = 0
         self._done = False
-        self._current_tick = self._start_tick
-        self._last_trade_tick = self._current_tick - 1
-        self._total_profit = 1.  # unit
-        self._first_rendering = True
-        self.history = {}
+
+        self._current_tick = random.randrange(0, self.terminal_idx - 1)
         return self._get_observation()
 
     def close(self):
@@ -88,10 +102,10 @@ class TradingEnv(gym.Env):
         self.current_price = self.df.iloc[self._current_tick, :]['close']
 
         #grab reward
-        reward = self._calculate_reward(action, self.current_price)
+        reward = self._calculate_reward(action)
 
         #grab current params
-        observation = self._get_observation(self._current_tick)
+        observation = self._get_observation()
 
         self.done = self.check_terminal()
 
@@ -99,8 +113,6 @@ class TradingEnv(gym.Env):
         self.store["reward_store"].append(reward)
         self.store["running_capital"].append(self.running_capital)
         info = self.store
-
-        
 
         return observation, reward, self._done, info
 
@@ -161,32 +173,14 @@ class TradingEnv(gym.Env):
         else:
             return False
 
-    def _get_observation(self, idx):
-        state = self.df[idx][1:]
-        state = self.scaler.transform(state.reshape(1, -1))
-        state = np.concatenate([state, [[self.portfolio_value/self.initial_capital, self.running_capital/self.portfolio_value, self.token_balance * self.current_price/self.initial_capital]]], axis=-1)
-        return state
+    def _get_observation(self):
+        price = np.array([self.prices[self._current_tick]])
+        state = self.signal_features[self._current_tick]
+        state = np.concatenate([state, [self.portfolio_value/self.initial_capital, self.running_capital/self.portfolio_value, self.token_balance * self.current_price/self.initial_capital]])
+        print(len(state))
+        print(len(self.selected_feature_name))
 
-    def render(self):
-        plt.clf()
-        plt.xlabel('trading days')
-        plt.ylabel('profit')
-        plt.plot(self._profit_history)
-        plt.savefig(self.save_path + str(self._env_id) + "-profit.png")
+        state = {self.selected_feature_name[i]: state[i] for i in range(len(state))}
 
-        plt.clf()
-        plt.xlabel('trading days')
-        plt.ylabel('close price')
-        window_ticks = np.arange(len(self._position_history))
-        eps_price = self.raw_prices[self._start_tick:self._end_tick + 1]
-        plt.plot(eps_price)
+        return price, state
 
-        short_ticks = []
-        long_ticks = []
-        flat_ticks = []
-
-        plt.plot(long_ticks, eps_price[long_ticks], 'g^', markersize=3, label="Long")
-        plt.plot(flat_ticks, eps_price[flat_ticks], 'bo', markersize=3, label="Flat")
-        plt.plot(short_ticks, eps_price[short_ticks], 'rv', markersize=3, label="Short")
-        plt.legend(loc='upper left', bbox_to_anchor=(0.05, 0.95))
-        plt.savefig(self.save_path + str(self._env_id) + '-price.png')
